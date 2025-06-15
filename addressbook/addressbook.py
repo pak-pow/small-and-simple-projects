71a868ba244d0741bdcd76cfd278a899754c3953
veee <paulag10987@gmail.com>
2025-06-15 13:45:40 +0800

    Update addressbook.py

--- addressbook/addressbook.py
+++ addressbook/addressbook.py
@@ -30,7 +30,7 @@ class AddressBook:
     def delete(self):
         pass
 
-    def run(self):
+   def run(self):
 
         if not os.path.exists(self.FILENAME):
             self.init_file()
@@ -38,16 +38,62 @@ class AddressBook:
         else:
             pass
 
-        name = input("Enter Name: ")
-        gender = input("Enter gender: ")
-        age = input("Enter Age: ")
-        city = input("Enter City: ")
-        street = input("Enter Street: ")
-        phone = input("Enter Phone Number: ")
+        choices = {
 
-        self.append_input(name, gender, age, city, street, phone)
+            "1" : "Create Address",
+            "2" : "Read / List All Address ",
+            "3" : "Update Address By Name",
+            "4" : "Delete Address By Name",
+            "5" : "Search Address By Name",
+            "6" : "Exit",
 
+        }
 
+        for idx, x in choices.items():
+            print (f"\t{idx}.) {x}")
+
+        try:
+
+            player_choice = int(input("\nEnter choice: "))
+
+            if player_choice == 1:
+
+                name = input("Enter Name: ")
+                gender = input("Enter gender: ")
+
+                while True:
+
+                    try:
+                        age = int(input("Enter Age: "))
+                        break
+
+                    except ValueError:
+                        print("Invalid Input, Try Again")
+
+                city = input("Enter City: ")
+                street = input("Enter Street: ")
+                phone = input("Enter Phone Number: ")
+
+                self.append_input(name, gender, age, city, street, phone)
+
+            elif player_choice == "2":
+                pass
+
+            elif player_choice == "3":
+                pass
+
+            elif player_choice == "4":
+                pass
+
+            elif player_choice == "5":
+                pass
+
+            elif player_choice == "6":
+                pass
+
+        except ValueError:
+            print("Invalid Input, Try Again.")
+            
 if __name__ == "__main__":
     app = AddressBook()
     app.run()
